2f4f334a28c36a0a7215057e5106136685a50ec7
Javeriya Saeed Abbasi <jiyasaeed333@gmail.com>
2021-06-18 03:23:25 +0500

    bgcolor
    added bg color

--- src/components/color.js
+++ src/components/color.js
@@ -4,7 +4,7 @@ function Color (props) {
     return (
         <div>
 
-            <h1>Question: Guess the color name?</h1>
+            <h1 >Question: Guess the color name?  </h1>
             <h3> Answer: This color is {props.colorName}. </h3>
          
             <hr/>
